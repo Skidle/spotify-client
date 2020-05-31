dc75c44aefcb4dd4dcbdd274907be7cfd5e553db
Daria Doronina <skidle.dd@gmail.com>
2020-05-31 20:27:04 +0200

    added airbnb config for eslint

--- src/App.jsx
+++ src/App.jsx
@@ -6,23 +6,27 @@ function App() {
   const request = new Request('https://api.spotify.com/v1/albums/4aawyAB9vmqN3uQ7FjRGTy/tracks?market=ES&limit=10&offset=5', {
     method: 'GET',
     headers: {
-      'Accept': 'application/json',
+      Accept: 'application/json',
       'Content-Type': 'application/json',
-      'Authorization': 'Bearer BQBGD_HWjJUBoRtcPXl0u-vAN35z-TfCuSCMTyfdhKOIIIzenK1TLXKUnay8gFxqpAOBxxeAX-X1Jrx-lfx1jtcsFHnf06ZFf3o_M3DIj0J7YQ6Aga8-UURJK_fqLTy-9xoy77AIpWKsUYKd9wZAHtbwnSiIdJPe0Gg'
-    }
+      Authorization: 'Bearer BQBGD_HWjJUBoRtcPXl0u-vAN35z-TfCuSCMTyfdhKOIIIzenK1TLXKUnay8gFxqpAOBxxeAX-X1Jrx-lfx1jtcsFHnf06ZFf3o_M3DIj0J7YQ6Aga8-UURJK_fqLTy-9xoy77AIpWKsUYKd9wZAHtbwnSiIdJPe0Gg',
+    },
   });
   console.log(request);
 
   fetch(request)
-  .then(response => response.json())
-  .then(json => console.log(json));
+    .then((response) => response.json())
+    .then((json) => console.log(json));
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit
+          {' '}
+          <code>src/App.js</code>
+          {' '}
+          and save to reload.
         </p>
         <a
           className="App-link"
