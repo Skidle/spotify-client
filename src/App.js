f78201c232b81fc747395e5334e34ffec7639459
Daria Doronina <skidle.dd@gmail.com>
2020-05-31 20:08:01 +0200

    api test get tracks for album, no authorization

--- src/App.js
+++ src/App.js
@@ -3,6 +3,20 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const request = new Request('https://api.spotify.com/v1/albums/4aawyAB9vmqN3uQ7FjRGTy/tracks?market=ES&limit=10&offset=5', {
+    method: 'GET',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer BQBGD_HWjJUBoRtcPXl0u-vAN35z-TfCuSCMTyfdhKOIIIzenK1TLXKUnay8gFxqpAOBxxeAX-X1Jrx-lfx1jtcsFHnf06ZFf3o_M3DIj0J7YQ6Aga8-UURJK_fqLTy-9xoy77AIpWKsUYKd9wZAHtbwnSiIdJPe0Gg'
+    }
+  });
+  console.log(request);
+
+  fetch(request)
+  .then(response => response.json())
+  .then(json => console.log(json));
+
   return (
     <div className="App">
       <header className="App-header">
