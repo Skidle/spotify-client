3086d5d55730a034bf7a8c0c78d4af529595d4ac
Daria Doronina <skidle.dd@gmail.com>
2020-06-04 20:13:51 +0200

    updated redirect uri

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,7 @@
 const CLIENT_ID = '81f8ff2fd1fa4537b7fc19a86ff6175a';
-const REDIRECT_URI = 'http://spotify-client.skidledd.now.sh';
+const REDIRECT_URI = process.env.NODE_ENV === 'development'
+  ? 'http://localhost:3000'
+  : 'http://spotify-client.skidledd.now.sh';
 export const AUTHORIZE_URL = `https://accounts.spotify.com/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=token&state=421`;
 
 export const SPOTIFY_API = 'https://api.spotify.com/v1/';
