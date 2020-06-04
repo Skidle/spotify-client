04c7a38ab788f80dc2558ec75fef9681d0655e6e
Daria Doronina <skidle.dd@gmail.com>
2020-06-04 11:09:41 +0200

    fixed redirect uri

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,5 @@
 const CLIENT_ID = '81f8ff2fd1fa4537b7fc19a86ff6175a';
-const REDIRECT_URI = 'http:%2F%2Flocalhost:3000';
+const REDIRECT_URI = 'http%3A%2F%2Fspotify-client-n59t31xir.now.sh%2F';
 export const AUTHORIZE_URL = `https://accounts.spotify.com/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=token&state=421`;
 
 export const SPOTIFY_API = 'https://api.spotify.com/v1/';
