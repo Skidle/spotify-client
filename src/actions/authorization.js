6a0e936ff25e01d0f5ce05bd80d0c5b2f21fd3ae
Daria Doronina <skidle.dd@gmail.com>
2020-06-05 03:35:06 +0200

    added accessToken to localstorage

--- src/actions/authorization.js
+++ src/actions/authorization.js
@@ -1,10 +1,12 @@
 import { USER_AUTHORIZED } from './actionTypes';
 import { getUrlAccessToken } from '../utils';
+import { ACCESS_TOKEN_STORAGE_KEY } from '../constants';
 
 export const authorizeUser = dispatch => {
   const accessToken = getUrlAccessToken();
 
   if (accessToken) {
+    global.localStorage.setItem(ACCESS_TOKEN_STORAGE_KEY, accessToken);
     dispatch({ type: USER_AUTHORIZED, accessToken });
   }
 };
--- src/constants.js
+++ src/constants.js
@@ -10,3 +10,5 @@ export const GET_CATEGORIES = `${SPOTIFY_API}browse/categories?limit=10&country=
 export const STATUS_FETCHING = 'fetching';
 export const STATUS_SUCCESS = 'success';
 export const STATUS_FAILURE = 'failure';
+
+export const ACCESS_TOKEN_STORAGE_KEY = 'accessToken';
